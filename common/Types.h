81d30f0d819b4f125e2fc2f083c996d0d54a1ae4
Valentín Bersi <valentinbersi@gmail.com>
2024-10-21 19:13:38 -0300

    Added Callable Type to Types.h

--- common/Types.h
+++ common/Types.h
@@ -16,3 +16,6 @@ template <typename Key, typename Value, typename Hash = std::hash<Key>,
           typename Pred = std::equal_to<Key>,
           typename Alloc = std::allocator<std::pair<const Key, Value>>>
 using HashMap = std::unordered_map<Key, Value, Hash, Pred, Alloc>;
+
+template <typename... Args>
+using Callable = std::function<void(Args...)>;
