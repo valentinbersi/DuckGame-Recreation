95528acc1dc905ec41274fdee5b00fdcabae878f
Valentín Bersi <valentinbersi@gmail.com>
2024-10-21 19:16:01 -0300

    Added observer pattern classes

---
+++ server/game/event/Subject.cpp
@@ -0,0 +1,44 @@
+#include "Subject.h"
+
+#define INVALID_EVENT                                                                    \
+    "The signal you are trying to connect/call has a different signature.\n"             \
+    "If you are sending the arguments correctly to connect()/emit(), either explicitly " \
+    "specify the template parameters of the method or use explicit construction of the " \
+    "type you register the signal with. For example: if you registered the signal with " \
+    "std::string and then sent a string literal to connect()/emit(), it will be "        \
+    "interpreted as a const char* instead of std::string."
+
+Subject::UnregisteredEvent::UnregisteredEvent(const std::string& eventName):
+        std::invalid_argument("Event " + eventName + " is not registered for this subject") {}
+
+Subject::Subject() = default;
+
+Subject::Subject(const Subject& other) = default;
+
+Subject& Subject::operator=(const Subject& other) {
+    if (this == &other)
+        return *this;
+
+    events = other.events;
+    return *this;
+}
+
+Subject::Subject(Subject&& other) noexcept: events(std::move(other.events)) {}
+
+Subject& Subject::operator=(Subject&& other) noexcept {
+    if (this == &other)
+        return *this;
+
+    events = std::move(other.events);
+    return *this;
+}
+
+Subject::AlreadyRegisteredEvent::AlreadyRegisteredEvent(const std::string& eventName):
+        std::invalid_argument("Event " + eventName + " is already registered for this subject") {}
+
+Subject::InvalidSignal::InvalidSignal(): std::runtime_error(INVALID_EVENT) {}
+
+void Subject::unregisterEvent(const std::string& name) {
+    if (const size_t deleted = events.erase(name); deleted == 0)
+        throw UnregisteredEvent(name);
+}
