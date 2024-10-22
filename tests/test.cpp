6991b77930534b2e1cad1b52a8af03b37f0c7255
ColinaAndres <andrescolina725@gmail.com>
2024-10-22 16:54:27 -0300

    Testing: Test comunicacion Multiples mensajes. Exito
    test para verificar que el protocolo puede recibir multiples
    mensajes en el mismo orden en el que se los manda un unico cliente.

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,11 +13,12 @@
 #include "ListenerSocket.h"
 #include <thread>
 
+#include <list>
 // using ::testing::AllOf;
 // using ::testing::HasSubstr;
 // using ::testing::ThrowsMessage;
 
-TEST(StandardProtocolTest, ClientToServerSendBytes) {
+TEST(ProtocolTest, ClientToServerSend) {
 
     ClientMessage clientMsg (MessageType::Input, InputAction::LEFT);
 
@@ -36,7 +37,36 @@ TEST(StandardProtocolTest, ClientToServerSendBytes) {
     EXPECT_EQ(recvMsg.type, clientMsg.type);
     EXPECT_EQ(recvMsg.action, clientMsg.action);
     
+    client.join();
     }
+
+TEST(ProtocolTest, ClientToServerMultipleSends) {
+    std::list<ClientMessage> list;
+    for(int i=0; i<50; i++){
+        list.push_back(ClientMessage(MessageType::Input, InputAction::LEFT));
+        list.push_back(ClientMessage(MessageType::Input, InputAction::RIGHT));
+    }
+
+    std::thread client ([list](){
+        ActiveSocket sktClient("localhost", "8080");
+        ClientSendProtocol sendProt (sktClient);
+        for(auto iter=list.begin(); iter!=list.end(); iter++){
+            sendProt.sendMessage(*iter);
+        }
+    });
+
+    ListenerSocket skt("8080");
+    ActiveSocket peer = skt.accept();
+    ServerRecvProtocol recvProt (peer);
+
+    for (auto iter=list.begin(); iter!=list.end(); iter++){
+        ClientMessage recvMsg = recvProt.receiveMessage();
+        EXPECT_EQ(iter->type, recvMsg.type);
+        EXPECT_EQ(iter->action, recvMsg.action);
+    }
+    
+    client.join();
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
