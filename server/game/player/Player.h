6824c6327f6cb28459253940746a73308ccc9e80
Valentín Bersi <valentinbersi@gmail.com>
2024-10-22 19:26:04 -0300

    Added Player class with methods to test input and match creation

---
+++ server/game/player/Player.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Input.h"
+
+class Player /*: public ¿Object2D? */ {
+    Input input;
+
+public:
+    Player();
+
+    /**
+     * Move the player to the right until stopMoveRight() is called
+     */
+    void moveRight();
+
+    /**
+     * Stops the player from moving to the right, if moveRight() was not called before this method,
+     * it does nothing
+     */
+    void stopMoveRight();
+
+    /**
+     * Move the player to the left until stopMoveLeft() is called.
+     */
+    void moveLeft();
+
+    /**
+     * Stops the player from moving to the left if moveLeft() was not called before this method, it
+     * does nothing
+     */
+    void stopMoveLeft();
+};
