3a12fb55a00f55b2f3845128a7ceac09cef49662
Valentín Bersi <valentinbersi@gmail.com>
2024-10-22 23:13:56 -0300

    Added Game Status signature

--- common/game/GameStatus.hpp
+++ common/game/GameStatus.hpp
@@ -1,3 +1,17 @@
 #pragma once
+#include "ObjectData.h"
 
-class GameStatus {};
+/**
+ * A struct with the current game status.
+ */
+class GameStatus {
+    std::vector<std::unique_ptr<ObjectData>> _gameObjects;
+    // Time matchTime
+
+public:
+    GameStatus(std::initializer_list<std::unique_ptr<ObjectData>> objects /*, Time matchTime*/);
+
+    [[nodiscard]] const std::vector<std::unique_ptr<ObjectData>>& gameObjects() const;
+
+    // [[nodiscard]] Time matchTime() const;
+};
