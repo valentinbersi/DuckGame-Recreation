6011a20a0902b22f644d6b02b73179d5fe846e2e
Valentín Bersi <valentinbersi@gmail.com>
2024-10-23 13:29:13 -0300

    Implemented GameStatus methods

--- common/game/GameStatus.cpp
+++ common/game/GameStatus.cpp
@@ -1,5 +1,8 @@
-//
-// Created by valen on 19/10/2024.
-//
-
 #include "GameStatus.hpp"
+
+GameStatus::GameStatus(const std::initializer_list<std::unique_ptr<GameObjectData>> objects):
+        _gameObjects(objects) {}
+
+const std::vector<std::unique_ptr<GameObjectData>>& GameStatus::gameObjects() const {
+    return _gameObjects;
+}
