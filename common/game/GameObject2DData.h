136c7ae76bb199819bfa00ff5b391156d2479733
Valentín Bersi <valentinbersi@gmail.com>
2024-10-23 11:27:03 -0300

    Documented: GameObject2DData

--- common/game/GameObject2DData.h
+++ common/game/GameObject2DData.h
@@ -3,6 +3,9 @@
 #include "GameObjectData.h"
 #include "Vector2.h"
 
+/**
+ * Relevant data for rendering a 2D object in the game
+ */
 class GameObject2DData: public GameObjectData {
     Vector2 _position;
     f32 _rotation;
@@ -15,9 +18,23 @@ public:
     GameObject2DData& operator=(GameObject2DData&& other) noexcept;
     ~GameObject2DData() override;
 
+    /**
+     * Construct a GameObject2DData with the given id, position and rotation
+     * @param id The id of the GameObject2D
+     * @param position The position of the GameObject2D
+     * @param rotation The rotation of the GameObject2D
+     */
     GameObject2DData(GameObjectID id, Vector2 position, f32 rotation);
 
+    /**
+     * Get the position of the 2D object
+     * @return The position of the 2D object
+     */
     [[nodiscard]] const Vector2& position() const;
 
+    /**
+     * Get the rotation of the 2D object
+     * @return The rotation of the 2D object
+     */
     [[nodiscard]] f32 rotation() const;
 };
