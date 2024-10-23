0e1872a2bb41ace42d512df9f651c718b635cb5c
Valentín Bersi <valentinbersi@gmail.com>
2024-10-23 12:17:58 -0300

    Implemented and documented DuckData

--- common/game/DuckData.cpp
+++ common/game/DuckData.cpp
@@ -1,1 +1,75 @@
 #include "DuckData.h"
+
+#define HAS_ARMOR_INDEX 0
+#define IS_PLAYING_DEAD_INDEX 1
+#define IS_CROUCHING_INDEX 2
+#define IS_IN_AIR_INDEX 3
+#define IS_FLAPPING_INDEX 4
+
+DuckData::DuckData(const DuckData& other):
+        GameObject2DData(other),
+        _id(other._id),
+        _life(other._life),
+        _gun(new GunData(*other._gun)),
+        _actions(other._actions) {}
+
+DuckData& DuckData::operator=(const DuckData& other) {
+    if (this == &other)
+        return *this;
+
+    GameObject2DData::operator=(other);
+    _id = other._id;
+    _life = other._life;
+    _gun = new GunData(*other._gun);
+    _actions = other._actions;
+    return *this;
+}
+
+DuckData::DuckData(DuckData&& other) noexcept:
+        GameObject2DData(std::move(other)),
+        _id(other._id),
+        _life(other._life),
+        _gun(other._gun),
+        _actions(other._actions) {
+    other._gun = nullptr;
+}
+
+DuckData& DuckData::operator=(DuckData&& other) noexcept {
+    if (this == &other)
+        return *this;
+
+    GameObject2DData::operator=(std::move(other));
+    _id = other._id;
+    _life = other._life;
+    _gun = other._gun;
+    other._gun = nullptr;
+    _actions = other._actions;
+    return *this;
+}
+
+DuckData::~DuckData() { delete _gun; }
+
+DuckData::DuckData(const GameObjectID objectID, Vector2 position, const f32 rotation,
+                   const DuckID duckID, const u8 life, std::unique_ptr<GunData> gun,
+                   const DuckActions actions):
+        GameObject2DData(objectID, std::move(position), rotation),
+        _id(duckID),
+        _life(life),
+        _gun(gun.release()),
+        _actions(actions) {}
+
+DuckID DuckData::duckID() const { return _id; }
+
+u8 DuckData::life() const { return _life; }
+
+const GunData& DuckData::gun() const { return *_gun; }
+
+bool DuckData::hasArmor() const { return _actions.test(HAS_ARMOR_INDEX); }
+
+bool DuckData::isPlayingDead() const { return _actions.test(IS_PLAYING_DEAD_INDEX); }
+
+bool DuckData::isCrouching() const { return _actions.test(IS_CROUCHING_INDEX); }
+
+bool DuckData::isInAir() const { return _actions.test(IS_IN_AIR_INDEX); }
+
+bool DuckData::isFlapping() const { return _actions.test(IS_FLAPPING_INDEX); }
--- common/game/DuckData.h
+++ common/game/DuckData.h
@@ -9,39 +9,93 @@ enum class DuckID {
     // enummerate duck colors
 };
 
-typedef u8 DuckFlags;
+typedef u8 DuckActions;
 
+/**
+ * Relevant data for rendering a duck
+ */
 class DuckData final: public GameObject2DData {
-    constexpr static u8 NFLAGS = 6;
+    constexpr static u8 NFLAGS = 5;
 
     DuckID _id;
     u8 _life;
-    GunData _gun;
-    std::bitset<NFLAGS> _flags;
+    GunData* _gun;
+    std::bitset<NFLAGS> _actions;
 
 public:
-    constexpr static u8 HAS_ARMOR = 0b1;
-    constexpr static u8 IS_PLAYING_DEAD = 0b01;
-    constexpr static u8 IS_CROUCHING = 0b001;
-    constexpr static u8 IS_IN_AIR = 0b0001;
-    constexpr static u8 IS_FLAPPING = 0b00001;
+    DuckData() = delete;
+    DuckData(const DuckData& other);
+    DuckData& operator=(const DuckData& other);
+    DuckData(DuckData&& other) noexcept;
+    DuckData& operator=(DuckData&& other) noexcept;
+    ~DuckData() override;
 
-    DuckData(u32 objectID, Vector2 position, f32 rotation, DuckID duckID, u8 life, GunData gun,
-             DuckFlags flags);
+    constexpr static DuckActions HAS_ARMOR = 0b1;
+    constexpr static DuckActions IS_PLAYING_DEAD = 0b01;
+    constexpr static DuckActions IS_CROUCHING = 0b001;
+    constexpr static DuckActions IS_IN_AIR = 0b0001;
+    constexpr static DuckActions IS_FLAPPING = 0b00001;
 
-    DuckID duckID() const;
+    /**
+     * Construct a DuckData object
+     * @param objectID the object's ID
+     * @param position the object's position
+     * @param rotation the object's rotation
+     * @param duckID the duck's ID
+     * @param life the life of the duck
+     * @param gun the gun of the duck
+     * @param actions the actions the duck is performing
+     */
+    DuckData(GameObjectID objectID, Vector2 position, f32 rotation, DuckID duckID, u8 life,
+             std::unique_ptr<GunData> gun, DuckActions actions);
 
-    u8 life() const;
+    /**
+     * Get the duckID of the duck
+     * @return the duckID of the duck
+     */
+    [[nodiscard]] DuckID duckID() const;
 
-    const GunData& gun() const;
+    /**
+     * Get the life of the duck
+     * @return the life of the duck
+     */
+    [[nodiscard]] u8 life() const;
 
-    bool hasArmor() const;
+    /**
+     * Get the gun of the duck \n
+     * WARNING: this is a reference to dynamic data, so once the DuckData is deleted, this reference
+     * will be invalid.
+     * @return the gun of the duck
+     */
+    [[nodiscard]] const GunData& gun() const;
 
-    bool isPlayingDead() const;
+    /**
+     * Check if the duck has armor
+     * @return true if the duck has armor, false otherwise
+     */
+    [[nodiscard]] bool hasArmor() const;
 
-    bool isCrouching() const;
+    /**
+     * Check if the duck is playing dead
+     * @return true if the duck is playing dead, false otherwise
+     */
+    [[nodiscard]] bool isPlayingDead() const;
 
-    bool isInAir() const;
+    /**
+     * Check if the duck is crouching
+     * @return true if the duck is crouching, false otherwise
+     */
+    [[nodiscard]] bool isCrouching() const;
 
-    bool isFlapping() const;
+    /**
+     * Check if the duck is in the air
+     * @return true if the duck is in the air, false otherwise
+     */
+    [[nodiscard]] bool isInAir() const;
+
+    /**
+     * Check if the duck is flapping
+     * @return true if the duck is flapping, false otherwise
+     */
+    [[nodiscard]] bool isFlapping() const;
 };
