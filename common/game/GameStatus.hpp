a928301be2136670d7a42dce54d8dfb8cceb0d14
Valentín Bersi <valentinbersi@gmail.com>
2024-10-23 17:57:42 -0300

    Documented GameStatus and added addObject() method

--- common/game/GameStatus.hpp
+++ common/game/GameStatus.hpp
@@ -9,7 +9,22 @@ class GameStatus {
     std::vector<std::unique_ptr<GameObjectData>> _gameObjects;
 
 public:
-    GameStatus(std::initializer_list<std::unique_ptr<GameObjectData>> objects);
+    GameStatus();
+    GameStatus(const GameStatus& other) = delete;
+    GameStatus& operator=(const GameStatus& other) = delete;
+    GameStatus(GameStatus&& other) noexcept;
+    GameStatus& operator=(GameStatus&& other) noexcept;
 
+    /**
+     * Add a game object to the game status.
+     * @param gameObject The game object to add.
+     * @return The game status.
+     */
+    GameStatus& addObject(std::unique_ptr<GameObjectData> gameObject);
+
+    /**
+     * Get the game objects.
+     * @return The game objects.
+     */
     [[nodiscard]] const std::vector<std::unique_ptr<GameObjectData>>& gameObjects() const;
 };
