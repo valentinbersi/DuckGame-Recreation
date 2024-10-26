08c13a55c16f153038aa3caba992a962b0aa39f2
ColinaAndres <andrescolina725@gmail.com>
2024-10-25 23:31:20 -0300

    Resolve Conflicts from stash pop
    conflict started from stash files from
    develop-ServerProtocol.

--- client/protocol/ClientRecvProtocol.h
+++ client/protocol/ClientRecvProtocol.h
@@ -2,8 +2,16 @@
 
 #include "ActiveSocket.h"
 #include "ReceiveProtocol.h"
+#include "Types.h"
 
-class ClientRecvProtocol : public  ReceiveProtocol{
+class ClientRecvProtocol: public ReceiveProtocol {
+private: 
+    HashMap<GameObjectID, std::function<std::unique_ptr<GameObjectData>()>> idsMap;
+
+    std::unique_ptr<GameObjectData> recvData();
+
+    std::unique_ptr<GameObjectData> recvDuckData();
+    
 public:
     ClientRecvProtocol(ActiveSocket&);
 
