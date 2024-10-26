38c3779e20987ba321439f22296612a0bd6281b0
ColinaAndres <andrescolina725@gmail.com>
2024-10-25 23:17:53 -0300

    Modified sendDuck in ServerSendProtocol
    Two more methods use in sendDuck to separate
    sending data from 2DData and DuckData

--- server/protocol/ServerSendProtocol.cpp
+++ server/protocol/ServerSendProtocol.cpp
@@ -1,6 +1,7 @@
 
 #include "ServerSendProtocol.h"
 #include "DuckData.h"
+#include "Math.h"
 #include <cstring>
 
 
@@ -8,12 +9,18 @@ ServerSendProtocol::ServerSendProtocol(ActiveSocket& socket): SendProtocol(socke
     idsMap[GameObjectID::Object2D] = [this](const GameObjectData& objData){
                                             sendDuck(objData);};
 }
+
 void ServerSendProtocol::sendGameObject2DData(const GameObject2DData* obj2Data){
-    
+    //NOTA:DESPUES TALVEZ DEBA MANDAR EL PROPIO ID, POR AHORA NO
+    sendInt(Math::floatToInteger(obj2Data->position.x()));
+    sendInt(Math::floatToInteger(obj2Data->position.y()));
+    sendInt(Math::floatToInteger(obj2Data->rotation));
 }
-void ServerSendProtocol::sendDuckData(const DuckData* objData){
-
-
+void ServerSendProtocol::sendDuckData(const DuckData* duckData){
+    sendByte(static_cast<unsigned char>(duckData->id));
+    sendByte(duckData->life);
+    sendByte(static_cast<unsigned char>(duckData->gun->id()));
+    sendByte(static_cast<u16>(duckData->extraData.to_ulong()));
 }
 
 void ServerSendProtocol::sendDuck(const GameObjectData& objData){
