4d51f188d7b859be0367b19a87109effe710e9f7
ColinaAndres <andrescolina725@gmail.com>
2024-10-27 01:47:52 -0300

    Implemented Communicator

--- client/comunicator/Communicator.h
+++ client/comunicator/Communicator.h
@@ -1,5 +1,24 @@
+#pragma once
 #include "Thread.h"
+#include "BlockingQueue.h"
+#include "ActiveSocket.h"
+#include "ComReceiver.h"
+#include "ComSender.h"
+
 class Communicator{
+private:
+    ActiveSocket skt;
+    BlockingQueue<ClientMessage> sendQueue;
+    BlockingQueue<GameStatus> recvQueue;
+    CommunicatorSender sender;
+    CommunicatorReceiver receiver;
 
+public:
+    Communicator(ActiveSocket&& socket);
 
+    bool trysend(ClientMessage& Message);
+
+    std::optional<GameStatus> tryrecv();
+
+    ~Communicator();
 };
