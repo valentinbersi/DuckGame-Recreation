315b89408fdf4c7aa7642ae5192c8a36f992bd08
ColinaAndres <andrescolina725@gmail.com>
2024-10-29 19:54:10 -0300

    fix: duplicate definition of Lobby Message

--- common/messages/LobbyMessage.h
+++ common/messages/LobbyMessage.h
@@ -1,12 +1,3 @@
-#pragma once
-#include "ClientMessage.h"
-
-struct LobbyMessage : public ClientMessage
-{
-    LobbyRequest request;
-    LobbyMessage(LobbyRequest request) : request(request), ClientMessage(MessageType::Lobby) {}
-    ~LobbyMessage() = default;
-};
 
 #pragma once
 #include "ClientMessage.h"
