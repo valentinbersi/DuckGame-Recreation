c2fb5edf67f35f33985e9e007e8715de3f2acdf9
Valentín Bersi <valentinbersi@gmail.com>
2024-10-30 00:32:35 -0300

    fix: unused parameter delta in GameController::update
    This is a temporary fix, this parameter will be used in the future

--- server/game/GameController.cpp
+++ server/game/GameController.cpp
@@ -8,7 +8,9 @@ GameController::GameController() = default;
 
 void GameController::start() { std::cout << "The game has started" << std::endl; }
 
-void GameController::update(float delta) { std::cout << "The game is running" << std::endl; }
+void GameController::update(const float delta) {
+    std::cout << "The game is running" << delta << std::endl;
+}
 
 void GameController::addPlayer(const PlayerID playerID) {
     const std::string id = std::to_string(playerID);
