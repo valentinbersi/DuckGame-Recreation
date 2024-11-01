35ea272bf111c771bc175b8036b893019a07b485
ColinaAndres <andrescolina725@gmail.com>
2024-10-31 23:33:00 -0300

    Modified: LobbyMessage has new atributes
    send in protocol works the same way, need to be adjusted

--- common/messages/LobbyMessage.h
+++ common/messages/LobbyMessage.h
@@ -5,63 +5,28 @@
 #include "Message.h"
 #include "Types.h"
 
-struct LobbyMessage: public Message {
+struct LobbyMessage : public Message {
     LobbyRequest request;
-    std::string usrName;
-    u16 matchId;
-
-    LobbyMessage() :
-            Message(MessageType::Lobby),
-            request(),
-            usrName(""),
-            matchId(0) {}
-    LobbyMessage(LobbyRequest request, std::string name, u16 id):
-            Message(MessageType::Lobby), request(request), usrName(name), matchId(id) {}
-    ~LobbyMessage() = default;
-};
-
-/*
-struct LobbyMessage_ : public Message {
-    LobbyRequest request;
+    u8 playerCount;
     std::string player1Name;
     std::string player2Name;
     u16 matchId;
-    u8 playerCount;
-    std::string mapChosen;
-
-    LobbyMessage_()
+   
+    LobbyMessage()
             : Message(MessageType::Lobby),
             request(),
             player1Name(""),
             player2Name(""),
             matchId(0),
-            playerCount(0),
-            mapChosen("") {}
+            playerCount(0) {}
 
-    LobbyMessage_(LobbyRequest request, std::string name1, std::string name2, u16 id, u8 count, std::string map)
+    LobbyMessage(LobbyRequest request, u8 count, std::string name1, std::string name2, u16 id)
             : Message(MessageType::Lobby),
             request(request),
+            playerCount(count),
             player1Name(name1),
             player2Name(name2),
-            matchId(id),
-            playerCount(count),
-            mapChosen(map) {}
+            matchId(id) {}
 
-    [[nodiscard]] LobbyRequest getRequest() const { return request; }
-    void setRequest(LobbyRequest req) { request = req; }
-
-    [[nodiscard]] std::string getPlayer1Name() const { return player1Name; }
-    void setPlayer1Name(const std::string& name) { player1Name = name; }
-
-    [[nodiscard]] std::string getPlayer2Name() const { return player2Name; }
-    void setPlayer2Name(const std::string& name) { player2Name = name; }
-
-    [[nodiscard]] u16 getMatchId() const { return matchId; }
-    void setMatchId(u16 id) { matchId = id; }
-
-    [[nodiscard]] u8 getPlayerCount() const { return playerCount; }
-    void setPlayerCount(u8 count) { playerCount = count; }
-
-    [[nodiscard]] std::string getMapChosen() const { return mapChosen; }
-    void setMapChosen(const std::string& map) { mapChosen = map; }
-};*/
+    ~LobbyMessage() = default;
+};
