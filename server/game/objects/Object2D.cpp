bc4f884388c28fa7c42f06f121579c72e91aaae6
Valentín Bersi <valentinbersi@gmail.com>
2024-11-03 13:04:56 -0300

    Revert "Several changes to Object2D class"
    This reverts commit 61ab8e25974879c38c35b4d253e0928e3f836f34.

--- server/game/objects/Object2D.cpp
+++ server/game/objects/Object2D.cpp
@@ -2,8 +2,33 @@
 
 #include <utility>
 
-Object2D::Object2D(Object* parent, Vector2 position, const float rotation):
-        Object(parent), _position(std::move(position)), _rotation(rotation) {}
+Object2D::Object2D(Object* parent): Object2D(parent, {0, 0}, 0) {}
+
+Object2D::Object2D(Object* parent, Vector2 position, const float rotation): Object(parent) {
+    _position = std::move(position);
+    _rotation = rotation;
+
+    const auto parent2D = dynamic_cast<Object2D*>(parent);
+
+    if (parent2D == nullptr) {
+        _globalPosition = _position;
+        _globalRotation = _rotation;
+        return;
+    }
+
+    if (position == Vector2::ZERO) {
+        _globalPosition = _position + parent2D->_globalPosition;
+        _globalRotation = _rotation + parent2D->_globalRotation;
+        return;
+    }
+
+    _globalPosition = parent2D->_globalPosition +
+                      Vector2::RIGHT.rotated(parent2D->_globalRotation) * position.length();
+
+    _globalRotation = _rotation + parent2D->_globalRotation;
+}
+
+Object2D::Object2D(): Object2D(nullptr) {}
 
 Object2D::Object2D(const Object2D& other) = default;
 
@@ -12,6 +37,8 @@ Object2D& Object2D::operator=(const Object2D& other) {
         return *this;
 
     Object::operator=(other);
+    _globalPosition = other._globalPosition;
+    _globalRotation = other._globalRotation;
     _position = other._position;
     _rotation = other._rotation;
     return *this;
@@ -19,6 +46,8 @@ Object2D& Object2D::operator=(const Object2D& other) {
 
 Object2D::Object2D(Object2D&& other) noexcept:
         Object(std::move(other)),
+        _globalPosition(std::move(other._globalPosition)),
+        _globalRotation(other._globalRotation),
         _position(std::move(other._position)),
         _rotation(other._rotation) {}
 
@@ -27,60 +56,59 @@ Object2D& Object2D::operator=(Object2D&& other) noexcept {
         return *this;
 
     Object::operator=(std::move(other));
+    _globalPosition = std::move(other._globalPosition);
+    _globalRotation = other._globalRotation;
     _position = std::move(other._position);
     _rotation = other._rotation;
     return *this;
 }
 
-Object2D::~Object2D() = default;
+Object2D::Object2D(Vector2 position, const float rotation):
+        Object2D(nullptr, std::move(position), rotation) {}
 
-Object2D& Object2D::globalPosition(Vector2 globalPosition) noexcept try {
-    const auto& parent2D = dynamic_cast<Object2D&>(parent());
+Object2D& Object2D::globalPosition(Vector2 globalPosition) noexcept {
+    _globalPosition = std::move(globalPosition);
 
-    _position = globalPosition - parent2D.globalPosition();
-    return *this;
+    const auto parent2D = dynamic_cast<Object2D*>(&parent());
 
-} catch (std::bad_cast&) {
-    _position = std::move(globalPosition);
-    return *this;
-}
+    if (parent2D == nullptr) {
+        _position = _globalPosition;
+        return *this;
+    }
 
-Vector2 Object2D::globalPosition() const noexcept try {
-    const auto& parent2D = dynamic_cast<Object2D&>(parent());
-
-    if (_position == Vector2::ZERO)
-        return parent2D.globalPosition();
-
-    return parent2D.globalPosition() +
-           Vector2::RIGHT.rotated(parent2D.globalRotation()) * position.length();
-
-} catch (std::bad_cast&) {
-    return _position;
-}
-
-Object2D& Object2D::globalRotation(const float globalRotation) noexcept try {
-    const auto& parent2D = dynamic_cast<Object2D&>(parent());
-
-    _rotation = globalRotation - parent2D.globalRotation();
-    return *this;
-} catch (std::bad_cast&) {
-    _rotation = globalRotation;
+    _position = _globalPosition - parent2D->_globalPosition;
     return *this;
 }
 
-float Object2D::globalRotation() const noexcept try {
-    const auto& parent2D = dynamic_cast<Object2D&>(parent());
+const Vector2& Object2D::globalPosition() const noexcept { return _globalPosition; }
 
-    if (_position == Vector2::ZERO)
-        return parent2D.globalRotation();
+Object2D& Object2D::globalRotation(const float globalRotation) noexcept {
+    _globalRotation = globalRotation;
 
-    return _rotation + parent2D.globalRotation();
-} catch (std::bad_cast&) {
-    return _rotation;
+    const auto parent2D = dynamic_cast<Object2D*>(&parent());
+
+    if (parent2D == nullptr) {
+        _rotation = _globalRotation;
+        return *this;
+    }
+
+    _rotation = _globalRotation - parent2D->_globalRotation;
+    return *this;
 }
+
+float Object2D::globalRotation() const noexcept { return _globalRotation; }
 
 Object2D& Object2D::position(Vector2 position) noexcept {
     _position = std::move(position);
+
+    const auto parent2D = dynamic_cast<Object2D*>(&parent());
+
+    if (parent2D == nullptr) {
+        _globalPosition = _position;
+        return *this;
+    }
+
+    _globalPosition = _position + parent2D->_globalPosition;
     return *this;
 }
 
@@ -88,6 +116,15 @@ const Vector2& Object2D::position() const noexcept { return _position; }
 
 Object2D& Object2D::rotation(const float rotation) noexcept {
     _rotation = rotation;
+
+    const auto parent2D = dynamic_cast<Object2D*>(&parent());
+
+    if (parent2D == nullptr) {
+        _globalRotation = _rotation;
+        return *this;
+    }
+
+    _globalRotation = _rotation + parent2D->_globalRotation;
     return *this;
 }
 
--- server/game/objects/Object2D.h
+++ server/game/objects/Object2D.h
@@ -4,18 +4,21 @@
 #include "Vector2.h"
 
 /**
- * 2D object with a position and a rotation
+ * 2D object with a position, rotation and scale
  */
 class Object2D: public Object {
+    Vector2 _globalPosition;
+    float _globalRotation;
+
     Vector2 _position;
     float _rotation;
 
 protected:
-    Object2D(const Object2D& other);
-    Object2D& operator=(const Object2D& other);
-    Object2D(Object2D&& other) noexcept;
-    Object2D& operator=(Object2D&& other) noexcept;
-
+    /**
+     * Initialize an Object2D with a parent
+     * @param parent The parent of the object
+     */
+    explicit Object2D(Object* parent);
     /**
      * Initialize an Object2D with a parent, a position a rotation and a scale
      * @param parent The parent of the object
@@ -28,8 +31,19 @@ public:
     /**
      * Initialize an Object2D with position (0, 0), rotation 0 and scale (1, 1)
      */
-    Object2D() = delete;
-    ~Object2D() override;
+    Object2D();
+    Object2D(const Object2D& other);
+    Object2D& operator=(const Object2D& other);
+    Object2D(Object2D&& other) noexcept;
+    Object2D& operator=(Object2D&& other) noexcept;
+
+    /**
+     * Initialize an Object2D with a position a rotation and a scale
+     * @param position The position
+     * @param rotation The rotation
+     * @param scale The scale
+     */
+    Object2D(Vector2 position, float rotation);
 
     /**
      * Set the global position of the object
@@ -42,7 +56,7 @@ public:
      * Get the global position of the object
      * @return The global position
      */
-    [[nodiscard]] Vector2 globalPosition() const noexcept;
+    [[nodiscard]] const Vector2& globalPosition() const noexcept;
 
     /**
      * Set the global rotation of the object
