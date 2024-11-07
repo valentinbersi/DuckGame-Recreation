5c016ca0f60c5fd45c49f838206b661121cf813a
Valentín Bersi <valentinbersi@gmail.com>
2024-11-07 11:43:14 -0300

    Removed unnecesary comment in Event.h

--- server/game/event/Event.h
+++ server/game/event/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <functional>  // If IDE shows this as unused include, don't remove it, otherwise server won't compile
+#include <functional>
 #include <list>
 #include <utility>
 
