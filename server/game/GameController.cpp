ae0e8156a29a1cb466c62f5339b5ad1d3cf6daea
Valentín Bersi <valentinbersi@gmail.com>
2024-11-07 18:40:28 -0300

    Removed loadChildren() from GameController and added limit to the amount of players

--- server/game/GameController.cpp
+++ server/game/GameController.cpp
@@ -12,16 +12,23 @@ GameController::GameController(): Object(nullptr){};
 
 void GameController::start() { std::cout << "The game has started" << std::endl; }
 
-void GameController::loadChildren() {}  // This should be empty
-
 void GameController::update(const float delta) {
     std::cout << "The game is running" << delta << std::endl;
 }
 
+#define FULL_GAME "The game is full"
+
+#define MAX_PLAYERS 4
+
 void GameController::addPlayer(const PlayerID playerID) {
     const std::string id = std::to_string(playerID);
 
-    addChild(id, std::make_unique<Player>());
+    if (players.size() >= MAX_PLAYERS)
+        throw std::logic_error(FULL_GAME);
+
+    auto duckID = static_cast<DuckID>(players.size());
+
+    addChild(id, std::make_unique<Player>(duckID));
     players[playerID] = dynamic_cast<Player*>(&getChild(id));
 }
 
