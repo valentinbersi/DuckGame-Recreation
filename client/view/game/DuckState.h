d4a68914957b44b5652b83850a2b77bfcde6f18f
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-11-19 13:34:07 -0300

    More sprites and music...
    and a new struct, DuckState

---
+++ client/view/game/DuckState.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct DuckState {
+    bool playingDead;
+    bool crouching;
+    bool inAir;
+    bool flapping;
+    bool beingDamaged;
+    bool movingRight;
+    bool movingLeft;
+    bool hasHelmet;
+    bool hasChestplate;
+    bool isShooting;
+};
