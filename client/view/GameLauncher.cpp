7425ffb8a62e1f0c867718e4dc02da348b8d7a11
Tomás Hevia <tomashevia27@gmail.com>
2024-11-18 23:14:36 -0300

    startGame button lock

--- client/view/GameLauncher.cpp
+++ client/view/GameLauncher.cpp
@@ -20,15 +20,9 @@ void GameLauncher::exec() {
 
     if (startGame) {
         try {
-            qDebug() << "twoPlayerLocal is" << twoPlayersLocal;
             Game game(communicator, twoPlayersLocal);
-            game.init();
-
-            // return 0;
-
         } catch (std::exception& e) {
             std::cerr << e.what() << std::endl;
-            // return 1;
         }
     }
 }
