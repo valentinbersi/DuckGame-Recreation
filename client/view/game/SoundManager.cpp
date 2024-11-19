0dbe65f68bdef42874052face842c8a288aa10ac
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2024-11-19 13:35:06 -0300

    Update of sound management in

--- client/view/game/SoundManager.cpp
+++ client/view/game/SoundManager.cpp
@@ -1,36 +1,20 @@
 #include "SoundManager.h"
 
-#define MUSIC_PATH "../assets/sounds/DuckGameOST.mp3"
-#define DEFAULT_VOLUME 40
+#define MUSIC_PATH "../assets/sounds/ost.ogg"
+#define DEFAULT_VOLUME 60
 
 SoundManager::SoundManager() {
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) std::cerr << "Error: " << Mix_GetError() << std::endl;
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 512) < 0) std::cerr << "Error: " << Mix_GetError() << std::endl;
     playMusic();
 }
 
 SoundManager::~SoundManager() {
-    for (auto& music : musicMap) {
-        Mix_FreeMusic(music.second);
-    }
-    musicMap.clear();
-
     for (auto& sound : soundMap) {
         Mix_FreeChunk(sound.second);
     }
 
     soundMap.clear();
     Mix_CloseAudio();
-}
-
-bool SoundManager::loadSound(GunID id) {
-    const char* path = soundMapIDS[id];
-    Mix_Chunk* sound = Mix_LoadWAV(path);
-    if (sound == nullptr) {
-        std::cerr << "Mix_LoadWAV Error: " << Mix_GetError() << std::endl;
-        return false;
-    }
-    soundMap[id] = sound;
-    return true;
 }
 
 void SoundManager::playMusic() {
@@ -44,5 +28,17 @@ void SoundManager::playSound(GunID id) {
     if (it == soundMap.end()) loadSound(id);
     it = soundMap.find(id);
 
+    Mix_VolumeChunk(it->second, 40);
     Mix_PlayChannel(-1, it->second, 0);
+}
+
+bool SoundManager::loadSound(GunID id) {
+    std::string path = soundMapIDS[id];
+    Mix_Chunk* sound = Mix_LoadWAV(path.c_str());
+    if (sound == nullptr) {
+        std::cerr << "Mix_LoadWAV Error: " << Mix_GetError() << std::endl;
+        return false;
+    }
+    soundMap[id] = sound;
+    return true;
 }
