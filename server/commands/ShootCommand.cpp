55118e422c754a5d4859b8a21ede61fce122b4d3
Valentín Bersi <valentinbersi@gmail.com>
2024-11-20 23:10:55 -0300

    Added [[maybe_unused]] to shoot command for the moment

--- server/commands/ShootCommand.cpp
+++ server/commands/ShootCommand.cpp
@@ -1,10 +1,10 @@
 #include "ShootCommand.h"
 
-ShootCommand::ShootCommand(const PlayerID id, InputAction action) : Command(id) , action(action) {}
+ShootCommand::ShootCommand(const PlayerID id, InputAction action): Command(id), action(action) {}
 
 void ShootCommand::execute(GameController& gameController) {
-    Player& player = gameController.getPlayer(id);
-    switch (id){
+    [[maybe_unused]] Player& player = gameController.getPlayer(id);
+    switch (id) {
         case InputAction::SHOOT_PRESSED:
             // return player.shoot();
         case InputAction::SHOOT_RELEASED:
@@ -12,5 +12,4 @@ void ShootCommand::execute(GameController& gameController) {
         default:
             return;
     }
-    
 }
