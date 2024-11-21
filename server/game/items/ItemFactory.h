a1134f3cde9af5580485390f09fc5394336817d1
Valentín Bersi <valentinbersi@gmail.com>
2024-11-20 23:11:40 -0300

    Added Item and ItemFactory class

--- server/game/items/ItemFactory.h
+++ server/game/items/ItemFactory.h
@@ -1,12 +1,20 @@
 #pragma once
-// #include "EquippableItem.h"
-// #include "unordered_map"
 
-// class ItemFactory {
-// private:
-//     static std::unordered_map<GunID, std::function<std::unique_ptr<EquippableItem>()>> factory;
-//     static void inicializeFactory();
+#include "Item.h"
+#include "unordered_map"
 
-// public:
-//     static std::unique_ptr<EquippableItem> createItem(GunID id);
-// };
+class ItemFactory {
+    /**
+     * HashMap used to create Item
+     */
+    static std::unordered_map<ItemID, std::function<std::unique_ptr<Item>()>> factory;
+
+public:
+    /**
+     * Creates Item base on given ItemID, throws exception
+     * in case id does not corresponde to any item
+     * @param id
+     * @return unique pointer to Created Item
+     */
+    static std::unique_ptr<Item> createItem(ItemID id);
+};
