01f9134a78dd0734453661837d5364003038a84b
valenbersi <valentinbersi@gmail.com>
2024-11-22 16:55:24 +0000

    Added dependencies to install

--- pre-install.sh
+++ pre-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Installing dependencies, sudo password may be required."
 sudo apt update
-sudo apt install -y build-essential cmake g++ nasm clang clang-format cpplint cppcheck pre-commit pkg-config alsa-base alsa-utils libasound2-dev qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools libqt5core5a libqt5gui5 libqt5widgets5 flex bison javacc libopus-dev opus-tools libopus0
+sudo apt install -y build-essential cmake g++ nasm clang clang-format cpplint cppcheck pre-commit pkg-config alsa-base alsa-utils libasound2-dev qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools libqt5core5a libqt5gui5 libqt5widgets5 flex bison javacc libopus-dev opus-tools libopus0 doxygen
 sudo apt upgrade -y
 echo "Pre dependencies installed"
