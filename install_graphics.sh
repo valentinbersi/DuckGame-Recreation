198aba5af77f7d8d32622ffb26683951eb83c7da
Valentín Bersi <valentinbersi@gmail.com>
2024-11-22 13:09:39 -0300

    Moved instalation of qt to pre-install.sh and added instalation of pkg-config and alsa libraries

--- install_graphics.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing graphics, sudo password may be required."
-sudo apt update
-sudo apt install -y qtbase5-dev qtbase5-dev-tools qttools5-dev qttools5-dev-tools libqt5core5a libqt5gui5 libqt5widgets5
-sudo apt upgrade -y
-echo "Graphics installed."
