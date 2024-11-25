8d326fb948f992ddea5333c7cc3da1ac3318abef
Valentín Bersi <valentinbersi@gmail.com>
2024-11-25 17:16:57 -0300

    Added utility include to EquippableWeapon.cpp

--- server/game/items/weapons/EquippableWeapon.cpp
+++ server/game/items/weapons/EquippableWeapon.cpp
@@ -1,5 +1,7 @@
 #include "EquippableWeapon.h"
 
+#include <utility>
+
 EquippableWeapon::EquippableWeapon(const ItemID id, const u8 ammo, Vector2 recoil,
                                    const float dispersion):
         ammo(ammo), id(id), firing(false), recoil(std::move(recoil)), dispersion(dispersion) {
