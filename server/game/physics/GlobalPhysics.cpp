cf8fadfaef1f3291812a649758e517d1fc6c4951
Valentín Bersi <valentinbersi@gmail.com>
2024-11-29 02:30:49 -0300

    Added configuration to gravity

--- server/game/physics/GlobalPhysics.cpp
+++ server/game/physics/GlobalPhysics.cpp
@@ -1,3 +1,7 @@
 #include "GlobalPhysics.h"
 
+#include "Config.h"
+
+const float GlobalPhysics::gravityScalar(Config::Physics::gravity());
+
 const Vector2 GlobalPhysics::gravity(Vector2(0, 1) * gravityScalar);
