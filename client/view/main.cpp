3680253baba3fa2840358e9946b50c69616a9527
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 01:53:01 -0300

    Implemented argument parsing on client

--- client/view/main.cpp
+++ client/view/main.cpp
@@ -1,15 +1,46 @@
 #include "GameLauncher.h"
+#include "Resource.h"
 
-int main(int argc, char* argv[]) {
-    cppstring hostname = argv[1];
-    cppstring servname = argv[2];
+int main(const int argc, char* argv[]) {
+    int opt;
+    while ((opt = getopt(argc, argv, "hr:")) != -1) {
+        switch (opt) {
+            case 'h':
+                std::cout << "Usage: " << argv[0] << " [-h] [-r resources_directory] hostname port"
+                          << std::endl;
+                return EXIT_SUCCESS;
+            case 'r': {
+                std::string resourcesPath(optarg);
+                if (resourcesPath.back() != '/')
+                    resourcesPath += '/';
+                Resource::get().setSearchPath(std::move(resourcesPath));
+            } break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-h] [-r resources_directory] hostname port"
+                          << std::endl;
+                return EXIT_FAILURE;
+        }
+    }
+
+    std::string hostname;
+    std::string port;
+
+    if (optind + 1 < argc) {
+        hostname = argv[optind];
+        port = argv[optind + 1];
+    } else {
+        std::cerr << "Usage: " << argv[0] << " [-h] [-r resources_directory] hostname port"
+                  << std::endl;
+        return EXIT_FAILURE;
+    }
 
     try {
-        GameLauncher launcher(argc, argv, hostname, servname);
+        GameLauncher launcher(argc, argv, hostname.c_str(), port.c_str());
         launcher.exec();
     } catch (const LibError& libError) {
-        syslog(LOG_CRIT, "%s", libError.what());
-        return -1;
+        std::cerr << libError.what() << std::endl;
+        return EXIT_FAILURE;
     }
+
     return 0;
 }
