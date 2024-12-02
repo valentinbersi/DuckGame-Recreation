6427f48a4886ae36888001e8e505b6e750badec3
Tomás Hevia <tomashevia27@gmail.com>
2024-12-02 00:40:57 -0300

    add background map

--- common/enums/Background.h
+++ common/enums/Background.h
@@ -1,13 +1,26 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 enum class BackgroundID {
-    CASCADE_CAVE,
-    CITY,
-    DARK_CAVE,
-    DESERT,
-    FOREST_DAY,
-    FOREST_NIGHT,
-    SNOWY_PEAKS,
-    SUNSET
+    CASCADE_CAVE = 0,
+    CITY = 1,
+    DARK_CAVE = 2,
+    DESERT = 3,
+    FOREST_DAY = 4,
+    FOREST_NIGHT = 5,
+    SNOWY_PEAKS = 6,
+    SUNSET = 7
 };
 
+const std::unordered_map<BackgroundID, std::string> backgrounds = {
+        {BackgroundID::CASCADE_CAVE, "assets/backgrounds/cascade_cave.png"},
+        {BackgroundID::CITY, "assets/backgrounds/city.png"},
+        {BackgroundID::DARK_CAVE, "assets/backgrounds/dark_cave.png"},
+        {BackgroundID::DESERT, "assets/backgrounds/desert.png"},
+        {BackgroundID::FOREST_DAY, "assets/backgrounds/forest_day.png"},
+        {BackgroundID::FOREST_NIGHT, "assets/backgrounds/forest_night.png"},
+        {BackgroundID::SNOWY_PEAKS, "assets/backgrounds/snowy_peaks.png"},
+        {BackgroundID::SUNSET, "assets/backgrounds/sunset.png"}
+};
