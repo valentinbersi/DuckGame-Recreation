18b8ef1122f4f03f97b5e6c7315a858cda6a1b6c
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 01:52:11 -0300

    Implemented argument parsing for editor

--- editor/main.cpp
+++ editor/main.cpp
@@ -1,9 +1,31 @@
 #include <QApplication>
+#include <iostream>
 
 #include "Object.h"
+#include "Resource.h"
 #include "ViewController.h"
 
 int main(int argc, char* argv[]) {
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hr:")) != -1) {
+        switch (opt) {
+            case 'h':
+                std::cout << "Usage: " << argv[0] << " [-h] [-r resources_directory]" << std::endl;
+                return 0;
+            case 'r': {
+                std::string resourcesPath(optarg);
+                if (resourcesPath.back() != '/')
+                    resourcesPath += '/';
+                Resource::get().setSearchPath(std::move(resourcesPath));
+
+            } break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " [-h] [-r resources_directory]" << std::endl;
+                return -1;
+        }
+    }
+
     QApplication a(argc, argv);
 
     /** registro la clase Object como tipo de Qt*/
