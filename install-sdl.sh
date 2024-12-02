879063ab39b7eef2a389c3f9cb7458522295b030
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 05:41:10 -0300

    Added installer of the game and error handling for dependencies installers

--- install-sdl.sh
+++ install-sdl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Installing SDL2"
-git clone git@github.com:libsdl-org/SDL.git
-cd SDL
+git clone git@github.com:libsdl-org/SDL.git || echo "Error: git clone failed, check your internet connection"
+cd SDL || echo "Error: SDL2 directory not found"
 git checkout release-2.30.9
-mkdir build
-cd build
-cmake ..
-cmake --build .
-sudo cmake --install .
+mkdir build || echo "Error: build directory could not be created or already exists" | exit 1
+cd build || echo "Error: build directory not found" | exit 1
+cmake .. || echo "Error: cmake configuring failed" | exit 1
+cmake --build . || echo "Error: cmake build failed" | exit 1
+sudo cmake --install . || echo "Error: cmake install failed" | exit 1
 echo "SDL2 installed"
 
