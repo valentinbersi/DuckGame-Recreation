aa07161d605d571b93749334a6ecd7968c944778
Tomás Hevia <tomashevia27@gmail.com>
2024-12-02 00:48:20 -0300

    add backgroung.cpp

--- common/enums/Background.h
+++ common/enums/Background.h
@@ -14,13 +14,4 @@ enum class BackgroundID {
     SUNSET = 7
 };
 
-const std::unordered_map<BackgroundID, std::string> backgrounds = {
-        {BackgroundID::CASCADE_CAVE, "assets/backgrounds/cascade_cave.png"},
-        {BackgroundID::CITY, "assets/backgrounds/city.png"},
-        {BackgroundID::DARK_CAVE, "assets/backgrounds/dark_cave.png"},
-        {BackgroundID::DESERT, "assets/backgrounds/desert.png"},
-        {BackgroundID::FOREST_DAY, "assets/backgrounds/forest_day.png"},
-        {BackgroundID::FOREST_NIGHT, "assets/backgrounds/forest_night.png"},
-        {BackgroundID::SNOWY_PEAKS, "assets/backgrounds/snowy_peaks.png"},
-        {BackgroundID::SUNSET, "assets/backgrounds/sunset.png"}
-};
+const static std::unordered_map<BackgroundID, std::string> backgrounds;
