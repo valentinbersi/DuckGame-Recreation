07f4cef61245238a83f72a0fb406d73d571f3ab4
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 22:52:01 -0300

    fix: installer now sets a new path for LD to search for libraries

--- install-sdl.sh
+++ install-sdl.sh
@@ -5,7 +5,7 @@ cd SDL || echo "Error: SDL2 directory not found"
 git checkout release-2.30.9
 mkdir build || echo "Error: build directory could not be created or already exists" | exit 1
 cd build || echo "Error: build directory not found" | exit 1
-cmake -DCMAKE_INSTALL_PREFIX=/usr .. || echo "Error: cmake configuring failed" | exit 1
+cmake .. || echo "Error: cmake configuring failed" | exit 1
 cmake --build . || echo "Error: cmake build failed" | exit 1
 sudo cmake --install . || echo "Error: cmake install failed" | exit 1
 echo "SDL2 installed"
