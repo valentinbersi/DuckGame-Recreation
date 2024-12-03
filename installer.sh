34df9f370be563909ead10337bab1017dccf0b55
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 23:56:12 -0300

    fix: installer not uploading the library cache

--- installer.sh
+++ installer.sh
@@ -1,5 +1,3 @@
-sudo echo 'export LD_LIBRARY_PATH=/src/local/lib/' >> ~/.bashrc
-sudo source ~/.bashrc
 ./pre-install.sh || echo "Error: Pre-install failed" | exit 1
 mkdir build
 cd build || echo "Error: build directory could not be created" | exit 1
@@ -7,3 +5,4 @@ cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 ./DuckGameTests || echo "Error: Tests failed, game won't be installed." | exit 1
 sudo cmake --install .
+sudo ldconfig
