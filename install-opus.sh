e3a172c7d6c560e8dac4e0f78e4726f2c0571813
Valentín Bersi <valentinbersi@gmail.com>
2024-12-02 21:42:03 -0300

    Added custom installation directory for all external libraries

--- install-opus.sh
+++ install-opus.sh
@@ -5,7 +5,7 @@ cd opus || echo "Error: opus directory not found"
 git checkout v1.5.2
 mkdir build || echo "Error: build directory could not be created or already exists" | exit 1
 cd build || echo "Error: build directory not found" | exit 1
-cmake -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" .. || echo "Error: cmake configuring failed" | exit 1
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" .. || echo "Error: cmake configuring failed" | exit 1
 cmake --build . || echo "Error: cmake build failed" | exit 1
 sudo cmake --install . || echo "Error: cmake install failed" | exit 1
 echo "Opus installed"
